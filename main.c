0bc865392836afe960f30d6ad07ebdc68832ffe6
James Stevenson <james.al.stevenson@gmail.com>
2020-05-24 21:04:41 +0100

    changed positional arg order to follow convention

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 int main(int argc, char *argv[]){
     FILE *fptr;
     char filename[PATH_MAX];
-    int i;
+
     if (argc != 3){
-        printf("Requires two arguments. <newfile> <newfile contents>");
+        printf("Requires two arguments. <newfile contents> <newfile>");
     }
-    printf("Writing %s writing to file %s\n", argv[2], argv[1]);
+    printf("Writing %s writing to file %s\n", argv[1], argv[2]);
     strcat(filename, "./");
-    strcat(filename, argv[1]);
+    strcat(filename, argv[2]);
     fptr = fopen(filename, "w+");
-    for (i = 0; i < strlen(argv[2]); ++i){
-        fputc(argv[2][i], fptr);
+    for (int i = 0; i < strlen(argv[1]); ++i){
+        fputc(argv[1][i], fptr);
     }
     fclose(fptr);
+
     return 0;
 }
